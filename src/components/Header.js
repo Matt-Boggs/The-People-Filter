06b6531665dd2e35d5b3cc3879f9835d4d7fc839
Matt Boggs <matt.david.boggs@gmail.com>
2020-11-23 17:47:52 -0700

    breaking up app into components

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = ()=>{
+
+    return (
+        <header>
+            The People Filter
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/ShowEmployees.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+const ShowEmployees = ()=>{
+
+
+    return (
+        <div className="container">
+            <div className="card">
+                <div className="card-title">
+                    Bob
+                </div>
+                <div className="card-body">
+                    Facts about bob
+                </div>
+                <div className="card-body">
+                    Facts about bob
+                </div>
+                <div className="card-body">
+                    Facts about bob
+                </div>
+            </div>
+            <div className="card">
+                <div className="card-title">
+                    Bob
+                </div>
+                <div className="card-body">
+                    Facts about bob
+                </div>
+                <div className="card-body">
+                    Facts about bob
+                </div>
+                <div className="card-body">
+                    Facts about bob
+                </div>
+            </div>
+            <div className="card">
+                <div className="card-title">
+                    Bob
+                </div>
+                <div className="card-body">
+                    Facts about bob
+                </div>
+                <div className="card-body">
+                    Facts about bob
+                </div>
+                <div className="card-body">
+                    Facts about bob
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default ShowEmployees
