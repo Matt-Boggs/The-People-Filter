a03178141e4b8e855b521d9685ca23ba9602f7c2
Matt Boggs <matt.david.boggs@gmail.com>
2020-11-24 14:59:09 -0700

    working sortbyname, need to hook it up to button and work with state

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -2,11 +2,29 @@ import React from "react";
 import {Col} from "react-bootstrap"
 
 const Buttons = (props)=>{
-    console.log(props.empArray)
+    const empArray = props.empArray
+
+    // list.sort((a, b) => (a.color > b.color) ? 1 : -1)
+
+    // console.log([...testArray].sort((a,b)=>(a.name > b.name) ? 1 : -1))
+
+    // Clicking the same button should reverse the sort
+
+    // sort by firstname
+    const nameSort = ()=>{
+        newArr = [...empArray].sort((a,b)=>(a.name > b.name) ? 1 : -1)
+        
+    }
+
+    // sort by lastname
+
+    // sort by unemployed status; change bg color of card for unemployed?
+
+    // some other sort?
     return(
         <>
         <Col xs={3} >
-            <button className="btn btn-primary">
+            <button className="btn btn-primary" >
                 sort by bla
             </button>
         </Col>
