bf3fd040d68f0cfb7f50f048cff5d932534111dd
Matt Boggs <matt.david.boggs@gmail.com>
2020-11-23 19:54:42 -0700

    using react bootstrap instead of divs with classnames

--- src/components/ShowEmployees.js
+++ src/components/ShowEmployees.js
@@ -1,53 +1,114 @@
 import React from "react";
-
+import { Container, Row, Col, Card,  } from "react-bootstrap"
 const ShowEmployees = ()=>{
 
 
     return (
-        <div className="container">
-            <div className="card">
-                <div className="card-title">
-                    Bob
+        <Container>
+            <button className="btn btn-success">button</button>
+            <Row>
+                <Col xs={1}></Col>
+
+                <Col className="card">
+                    <div className="card-body">
+                        <div className="card-title">
+                            bob
+                        </div>
+                        <div className="card-text">
+                            stuff about bob
+                        </div>
+                        <div className="card-text">
+                            stuff about bob
+                        </div>
+                        <div className="card-text">
+                            stuff about bob
+                        </div>
+                    </div>
+                </Col>
+                <Col className="card">
+                    <div className="card-body">
+                        <div className="card-title">
+                            bob
+                        </div>
+                        <div className="card-text">
+                            stuff about bob
+                        </div>
+                        <div className="card-text">
+                            stuff about bob
+                        </div>
+                        <div className="card-text">
+                            stuff about bob
+                        </div>
+                    </div>
+                </Col>
+                {/* <div className="card col-2">
+                    <div className="card-body">
+                        <div className="card-title">
+                            bob
+                        </div>
+                        <div className="card-text">
+                            stuff about bob
+                        </div>
+                        <div className="card-text">
+                            stuff about bob
+                        </div>
+                        <div className="card-text">
+                            stuff about bob
+                        </div>
+                    </div>
                 </div>
-                <div className="card-body">
-                    Facts about bob
+                <div className="card col-2">
+                    <div className="card-body">
+                        <div className="card-title">
+                            bob
+                        </div>
+                        <div className="card-text">
+                            stuff about bob
+                        </div>
+                        <div className="card-text">
+                            stuff about bob
+                        </div>
+                        <div className="card-text">
+                            stuff about bob
+                        </div>
+                    </div>
                 </div>
-                <div className="card-body">
-                    Facts about bob
+                <div className="card col-2">
+                    <div className="card-body">
+                        <div className="card-title">
+                            bob
+                        </div>
+                        <div className="card-text">
+                            stuff about bob
+                        </div>
+                        <div className="card-text">
+                            stuff about bob
+                        </div>
+                        <div className="card-text">
+                            stuff about bob
+                        </div>
+                    </div>
                 </div>
-                <div className="card-body">
-                    Facts about bob
-                </div>
-            </div>
-            <div className="card">
-                <div className="card-title">
-                    Bob
-                </div>
-                <div className="card-body">
-                    Facts about bob
-                </div>
-                <div className="card-body">
-                    Facts about bob
-                </div>
-                <div className="card-body">
-                    Facts about bob
-                </div>
-            </div>
-            <div className="card">
-                <div className="card-title">
-                    Bob
-                </div>
-                <div className="card-body">
-                    Facts about bob
-                </div>
-                <div className="card-body">
-                    Facts about bob
-                </div>
-                <div className="card-body">
-                    Facts about bob
-                </div>
-            </div>
-        </div>
+                <div className="card col-2">
+                    <div className="card-body">
+                        <div className="card-title">
+                            bob
+                        </div>
+                        <div className="card-text">
+                            stuff about bob
+                        </div>
+                        <div className="card-text">
+                            stuff about bob
+                        </div>
+                        <div className="card-text">
+                            stuff about bob
+                        </div>
+                    </div>
+                </div> */}
+
+                <Col xs={1}></Col>
+            </Row>
+        </Container>
     )
 }
 
